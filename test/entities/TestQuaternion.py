c58106bdf486ac3528d92b6920fbd4e38e52c5f9
Stefano Borini <stefano.borini@ferrara.linux.it>
2011-07-08 22:14:01 +0200

    test for quaternions.

--- test/entities/TestQuaternion.py
+++ test/entities/TestQuaternion.py
@@ -2,22 +2,20 @@
 import os; import sys; script_path=sys.path[0]; sys.path.append(os.path.join(script_path, "../../../"));
 import unittest
 
-from wavemol.math import geometry
-
 import math
 
 class TestQuaternion(unittest.TestCase):
-    def testInit(self): # fold>>
-        q = geometry.Quaternion.fromVector3D( geometry.Vector3D([1.0,2.0,3.0]), math.pi/4.0 )
+    def test_init(self): # fold>>
+        q = entities.Quaternion.from_vector( entities.Vector3D(1.0,2.0,3.0), math.pi/4.0 )
         
-        self.assertAlmostEqual(q.w(), math.cos(math.pi/8.0))
-        self.assertAlmostEqual(q.x(), 1.0*math.sin(math.pi/8.0))
-        self.assertAlmostEqual(q.y(), 2.0*math.sin(math.pi/8.0))
-        self.assertAlmostEqual(q.z(), 3.0*math.sin(math.pi/8.0))
+        self.assertAlmostEqual(q.w, math.cos(math.pi/8.0))
+        self.assertAlmostEqual(q.x, 1.0*math.sin(math.pi/8.0))
+        self.assertAlmostEqual(q.y, 2.0*math.sin(math.pi/8.0))
+        self.assertAlmostEqual(q.z, 3.0*math.sin(math.pi/8.0))
 
-    def testToRotationMatrix(self): 
-        q=geometry.Quaternion.fromVector3D(geometry.Vector3D([1.0,0.0,0.0]) , 0.0615)
-        m = q.toRotationMatrix()
+    def test_to_rotation_matrix(self): 
+        q = entities.Quaternion.from_vector(entities.Vector3D(1.0,0.0,0.0) , 0.0615)
+        m = transform.Rotation.from_quaternion(q)
         self.assertAlmostEqual(m[0][0], 1.0)
         self.assertAlmostEqual(m[0][1], 0.0)
         self.assertAlmostEqual(m[0][2], 0.0)
@@ -28,8 +26,8 @@ class TestQuaternion(unittest.TestCase):
         self.assertAlmostEqual(m[2][1], math.sin(0.0615))
         self.assertAlmostEqual(m[2][2], math.cos(0.0615))
 
-        q=geometry.Quaternion.fromVector3D(geometry.Vector3D([1.0,0.0,0.0]),-0.0615)
-        m = q.toRotationMatrix()
+        q = entities.Quaternion.from_vector(entities.Vector3D(1.0,0.0,0.0),-0.0615)
+        m = transform.Rotation.from_quaternion(q)
         self.assertAlmostEqual(m[0][0], 1.0)
         self.assertAlmostEqual(m[0][1], 0.0)
         self.assertAlmostEqual(m[0][2], 0.0)
